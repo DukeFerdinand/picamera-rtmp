e5f8cfe5be986ce55ef05bbc075a02d71c52d5c9
Doug Flynn <duke@botsu.ro>
2024-02-26 10:11:56 -0800

    Complete a basic FFMPEG pi camera rtmp stream

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import time
+
 from picamera2.encoders import H264Encoder, Quality
+from picamera2.outputs import FfmpegOutput
 from picamera2 import Picamera2
+
+# Video Parameters
+resolution = (1920, 1080)
+
 picam2 = Picamera2()
+#video_config = picam2.create_video_configuration(main={"size": resolution})
 video_config = picam2.create_video_configuration()
 picam2.configure(video_config)
+
 encoder = H264Encoder()
+output = FfmpegOutput("-c:v copy -f flv -flvflags no_duration_filesize rtmp://192.168.4.79/live/picamera")
+# output = FfmpegOutput("-f flv rtmp://192.168.4.79/live/picamera")
 
-picam2.start_recording(encoder, 'low.h264', quality=Quality.VERY_LOW)
-print(encoder._bitrate)
-time.sleep(5)
-picam2.stop_recording()
 
-picam2.start_recording(encoder, 'medium.h264', quality=Quality.MEDIUM)
-print(encoder._bitrate)
-time.sleep(5)
-picam2.stop_recording()
+if __name__ == "__main__":
+    try:
+        print("Starting RTMP stream!")
+        print(encoder.bitrate)
 
-picam2.start_recording(encoder, 'high.h264', quality=Quality.VERY_HIGH)
-print(encoder._bitrate)
-time.sleep(5)
-picam2.stop_recording()
+        picam2.start_recording(encoder=encoder, output=output, quality=Quality.VERY_HIGH)
+        while True:
+            print("recording is active...")
+            time.sleep(5)
+    except Exception as e:
+        print(e)
+        picam2.stop_recording()
